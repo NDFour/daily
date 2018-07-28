d55932993cd920889cfc14aad9022046b5b89b39
NDFour <ndfour@foxmail.com>
2018-07-28 17:36:52 +0800

    修改网页布局  修改数据库pan.movie.models.py

--- pandy/movie/models.py
+++ pandy/movie/models.py
@@ -3,13 +3,13 @@ from django.utils import timezone
 
 # Create your models here.
 class Movie(models.Model):
-    v_href = models.CharField('采集页链接', max_length=255)
+    v_href = models.CharField('采集页链接', max_length=255, blank=True)
     v_pic = models.CharField('封面图', max_length=255)
     v_name = models.CharField('电影名', max_length=255)
     v_bdpan = models.CharField('网盘链接', max_length=255)
-    v_pass = models.CharField('密码', max_length=255)
-    v_ed2k = models.CharField('磁力链接 1', max_length=255)
-    v_magnet = models.CharField('磁力链接 2', max_length=255)
+    v_pass = models.CharField('密码', max_length=255, blank=True)
+    v_ed2k = models.CharField('磁力链接 1', max_length=255, blank=True)
+    v_magnet = models.CharField('磁力链接 2', max_length=255, blank=True)
 
     v_pub_date = models.DateTimeField('最后更新时间', default=timezone.now())
 
