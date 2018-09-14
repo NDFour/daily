6576f292ef96f8f63f174251127f4b518147d90f
NDFour <lgang219@gmail.com>
2018-09-14 13:20:35 +0800

    增加影片分类

--- pandy/onlineplay/admin.py
+++ pandy/onlineplay/admin.py
@@ -3,10 +3,10 @@ from .models import Onlineplay
 
 # Register your models here.
 class OnlineplayAdmin(admin.ModelAdmin):
-    list_display = ('id', 'v_name', 'v_vip')
+    list_display = ('id', 'v_name', 'v_type', 'v_vip')
 
     fieldsets = [
-            ('基本信息', {'fields': ['v_name', 'v_pic', 'v_playurl', 'v_text_info', 'v_href', 'v_belong_to', 'v_vip']}),
+            ('基本信息', {'fields': ['v_name', 'v_pic', 'v_playurl', 'v_text_info', 'v_href', 'v_belong_to', 'v_type', 'v_vip']}),
             ('时间戳', {'fields': ['v_pub_date']}),
     ]
 
