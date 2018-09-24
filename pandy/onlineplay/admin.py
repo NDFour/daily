df916e133a9889c24ff7989f6ce34c10d28636f1
lynn <ndfoue@foxmail.com>
2018-09-24 20:30:16 +0800

    modefy

--- pandy/onlineplay/admin.py
+++ pandy/onlineplay/admin.py
@@ -6,7 +6,7 @@ class OnlineplayAdmin(admin.ModelAdmin):
     list_display = ('id', 'v_name', 'v_type', 'v_vip')
 
     fieldsets = [
-            ('基本信息', {'fields': ['v_name', 'v_pic', 'v_playurl', 'v_text_info', 'v_href', 'v_belong_to', 'v_type', 'v_vip']}),
+            ('基本信息', {'fields': ['v_name', 'v_pic', 'v_playurl', 'v_text_info', 'v_href', 'v_belong_to', 'v_type', 'v_vip', 'v_views']}),
             ('时间戳', {'fields': ['v_pub_date']}),
     ]
 
