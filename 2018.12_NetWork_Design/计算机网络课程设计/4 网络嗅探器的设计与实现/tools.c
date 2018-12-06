9b020f7a69b69560bcfc40bc0e3f0f174239da1c
lynn <ndfoue@foxmail.com>
2018-12-06 22:44:47 +0800

    snifer everything update

--- 2018.12_NetWork_Design/计算机网络课程设计/4 网络嗅探器的设计与实现/tools.c
+++ 2018.12_NetWork_Design/计算机网络课程设计/4 网络嗅探器的设计与实现/tools.c
@@ -3,7 +3,7 @@
  *         Author: Lynn
  *          Email: lgang219@gmail.com
  *         Create: 2018-12-05 16:34:10
- *  Last Modified: 2018-12-05 18:47:21
+ *  Last Modified: 2018-12-06 22:43:40
  */
 
 #include<stdio.h>
@@ -74,11 +74,11 @@ int snifer(int sock)
 
     n_read = recvfrom(sock, buffer, 2048, 0, NULL, NULL);
     /*
-    14  6(dest)+6(source)+2(type or length)
+    14  6(dest)+6(source)+2(type or length) // MAC dst(6) + src(6) + type(2)  (p96)
     +
-    20  ip header
+    20  ip header                           // IP static(20)
     +
-    8  icmp, tcp or udp header
+    8  icmp, tcp or udp header              // UDP static(8)
     = 42
     */
     if (n_read < 42)
@@ -90,41 +90,80 @@ int snifer(int sock)
     ethhead = buffer;
     p = ethhead;
     int n = 0XFF; // 255
-    printf("MAC:%.2X:%.02X:%02X:%02X:%02X:%02X=>"
-           "%.2X:%.2X:%.2X:%.2X:%.2X:%.2X\n",
-           p[6]&n, p[7]&n, p[8]&n, p[9]&n, p[10]&n, p[11]&n, p[0]&n, p[1]&n, p[2]&n, p[3]&n, p[4]&n, p[5]&n);
 
-    iphead = ethhead + 14;
-    p = iphead + 12;
+    // MAC address
+    // printf("MAC:%.2X:%.02X:%02X:%02X:%02X:%02X=>"
+    //        "%.2X:%.2X:%.2X:%.2X:%.2X:%.2X\n",
+    //        p[6]&n, p[7]&n, p[8]&n, p[9]&n, p[10]&n, p[11]&n, p[0]&n, p[1]&n, p[2]&n, p[3]&n, p[4]&n, p[5]&n);
+    int mac_src_addr[6];
+    int mac_dst_addr[6];
+    mac_src_addr[0] = p[6] & n;
+    mac_src_addr[1] = p[7] & n;
+    mac_src_addr[2] = p[8] & n;
+    mac_src_addr[3] = p[9] & n;
+    mac_src_addr[4] = p[10] & n;
+    mac_src_addr[5] = p[11] & n;
 
-    printf("IP:%d.%d.%d.%d => %d.%d.%d.%d\n",
-           p[0] & 0XFF, p[1] & 0XFF, p[2] & 0XFF, p[3] & 0XFF, p[4] & 0XFF, p[5] & 0XFF, p[6] & 0Xff, p[7] & 0XFF);
-    proto = (iphead + 9)[0];
-    p = iphead + 20;
+    mac_dst_addr[0] = p[0] & n;
+    mac_dst_addr[1] = p[1] & n;
+    mac_dst_addr[2] = p[2] & n;
+    mac_dst_addr[3] = p[3] & n;
+    mac_dst_addr[4] = p[4] & n;
+    mac_dst_addr[5] = p[5] & n;
+
+    iphead = ethhead + 14; // position IP frame
+    p = iphead + 12; // position src & dst
+
+    // IP address
+    // printf("IP:%d.%d.%d.%d => %d.%d.%d.%d\n",
+    //        p[0] & 0XFF, p[1] & 0XFF, p[2] & 0XFF, p[3] & 0XFF, p[4] & 0XFF, p[5] & 0XFF, p[6] & 0Xff, p[7] & 0XFF);
+    int ip_src_addr[4];
+    int ip_dst_addr[4];
+    ip_src_addr[0] = p[0] & n;
+    ip_src_addr[1] = p[1] & n;
+    ip_src_addr[2] = p[2] & n;
+    ip_src_addr[3] = p[3] & n;
+    ip_dst_addr[0] = p[4] & n;
+    ip_dst_addr[1] = p[5] & n;
+    ip_dst_addr[2] = p[6] & n;
+    ip_dst_addr[3] = p[7] & n;
+
+    proto = (iphead + 9)[0]; // position Protocol (p130)
+    p = iphead + 20; // position Variable
+    // Protocol
     printf("Protocol:");
-    switch (proto)
+    switch (proto) // int
     {
     case IPPROTO_ICMP:
-        printf("ICMP\n");
+        printf("\033[47;35mICMP\033[0m\n");
         break;
     case IPPROTO_IGMP:
-        printf("IGMP\n");
+        printf("\033[47;35mIGMP\033[0m\n");
         break;
     case IPPROTO_IPIP:
-        printf("IPIP\n");
+        printf("\033[47;35mIPIP\033[0m\n");
         break;
     case IPPROTO_TCP:
     case IPPROTO_UDP:
-        printf("%s,", proto == IPPROTO_TCP ? "TCP" : "UDP");
-        printf("source port:%u,", (p[0] << 8) & 0XFF00 | p[1] & 0XFF);
-        printf("dest port:%u\n", (p[2] << 8) & 0XFF00 | p[3] & 0XFF);
+        printf("\033[47;35m%s\033[0m,", proto == IPPROTO_TCP ? "TCP" : "UDP");
+        printf("source port:\033[;33m%u\033[0m,", (p[0] << 8) & 0XFF00 | p[1] & 0XFF);
+        printf("dest port:\033[;33m%u\033[0m\n", (p[2] << 8) & 0XFF00 | p[3] & 0XFF);
         break;
     case IPPROTO_RAW:
-        printf("RAW\n");
+        printf("\033[47;35mRAW\033[0m\n");
         break;
     default:
-        printf("Unkown, please query in include/linux/in.h\n");
+        printf("\033[47;41mUnkown\033[0m, please query in include/linux/in.h\n");
     }
+
+    // Output
+    printf("MAC:%.2X:%.02X:%02X:%02X:%02X:%02X => "
+           "%.2X:%.2X:%.2X:%.2X:%.2X:%.2X\n",
+           mac_src_addr[0], mac_src_addr[1], mac_src_addr[2], mac_src_addr[3], mac_src_addr[4], mac_src_addr[5], mac_dst_addr[0], mac_dst_addr[1], mac_dst_addr[2], mac_dst_addr[3], mac_dst_addr[4], mac_dst_addr[5]);
+
+    printf("IP:%d.%d.%d.%d => %d.%d.%d.%d\n",
+           ip_src_addr[0], ip_src_addr[1], ip_src_addr[2], ip_src_addr[3], ip_dst_addr[0], ip_dst_addr[1], ip_dst_addr[2], ip_dst_addr[3]);
+
     return 0;
 }
 
@@ -137,4 +176,5 @@ void showhelp()
     printf("  -p  Filte by Protocol\n");
     printf("  -h  Show this message\n");
     printf("\n");
+    printf(" sudo ./snifer -a 10.1.8.1 -m ff:ff:ff:ff -P 1080 -p TCP\n\n");
 }
