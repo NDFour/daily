a41c6ec7bda6ad427af487a7668e81283fba0dee
lynn <ndfour@foxmail.com>
2019-03-26 11:29:07 +0800

    修改 pandy 为每日更新资源脚本增加删除空白pic和playurl 代码

--- pandy/spider/autoUpdate.py
+++ pandy/spider/autoUpdate.py
@@ -34,7 +34,27 @@ def get_html():
         html_text = ''
     return html_text
 
+# 删除图片链接为空，和播放链接为空的数据项
+def rmNullItem():
+    conn = pymysql.connect('127.0.0.1', port=3306, user='root', password='cqmygpython2', db='bdpan', charset='utf8')
+    cursor = conn.cursor()
+
+    sqlList = []
+    rmNullPic = 'delete from onlineplay_onlineplay where length(v_pic)=0;'
+    rmNullPlayurl = 'delete from onlineplay_onlineplay where length(v_playurl)=0;'
+    sqlList.append(rmNullPic)
+    sqlList.append(rmNullPlayurl)
+    for i in sqlList:
+        try:
+            cursor.execute()
+            conn.commit()
+         except:
+            conn.rollback()
+            cursor.close()
+            conn.close()
+
 def main():
     get_update()
+    rmNullItem()
 
 main()
