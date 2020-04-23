37ec620b5ab15fee98ce29c559a7dc1e8075b87e
aliyun_hk <ndfour@foxmail.com>
2020-04-24 04:16:26 +0800

    [pandy] modefy robot.py to new url

--- pandy/robot.py
+++ pandy/robot.py
@@ -11,7 +11,7 @@ def subscribe(message):
 @robot.text
 def hello(message):
     rel_info_text = '📚你好，这个是自动回复\n\n[玫瑰]书籍名字可以不完整\n[凋谢]但绝不可以有错别字哦，会搜不到的 ！\n\n'
-    rel_info_a = '<a href="https://www.runner8.com/books/search/?book_name=' + message.content + '&book_search=book_search">点我查看[' + message.content + ']搜索结果</a>'
+    rel_info_a = '<a href="https://www.chenjin5.com/books/search/?book_name=' + message.content + '&book_search=book_search">点我查看[' + message.content + ']搜索结果</a>'
     return rel_info_text + rel_info_a
 
 
