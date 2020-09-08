456e0c4244e8ac36b9e6c55ec6be39b4a3bb7695
lynn <lynn@lynndeMacBook-Pro-3.local>
2020-09-09 04:30:35 +0800

    [Kindle_vocabularies] 修改为使用本地词库 ECDICT

--- Kindle_vocabularies/extract_vocab.py
+++ Kindle_vocabularies/extract_vocab.py
@@ -1,132 +1,181 @@
 import sqlite3
 import time
-import requests
+import os
+import stardict
 
 
-def get_voca():
-    conn = sqlite3.connect('vocab.db')
+'''
+1. 读取 生词本 数据库
+2. 与旧版本数据库对比，筛选出本次需要翻译的 “新”单词
+3. 将 “新”单词 写入 “旧”版本数据库
+4. 返回 “新”单词 列表
+
+param:
+    do_check: 是否检测 该词 已存在之前的单词数据库
+'''
+def get_voca(do_check):
+    conn = sqlite3.connect('kindle_db/vocab_2020_08_21.db')
     print('Open db succ')
 
     cursor = conn.cursor()
-    words_data = cursor.execute('SELECT word, stem, timestamp FROM WORDS')
+    words_data = cursor.execute('SELECT word FROM WORDS')
 
     word_list = []
     i_cnt = 0
     for word in words_data:
-        w = word[0]
-        if word[0] == word[1]:
-            s = ''
-        else:
-            s = word[1]
-        t = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime( word[2] ))
+        w = word[0].replace('"', '').replace("'", '').strip()
+
+        # 判断该词是否已存在于之前的生词本数据库中
+        if do_check:
+            if is_stored(w):
+                continue
+            insert_to_base(w)
+
         print(str(i_cnt) + '.   ' + w)
-        '''
-        print('stem: ' + s)
-        print('timestamp: ' + str(t))
-        print()
-        '''
-        word_list.append({'word': w, 'stem': w, 'timestamp': t, 'translate': ''})
+
+        word_list.append(w)
 
         i_cnt += 1
 
     conn.close()
     return word_list
 
+'''
+判断该词是否已存在于之前的生词本数据库中
+'''
+def is_stored(word):
+    conn = sqlite3.connect('kindle_db/vocab_2020_08_21.db')
+    cursor = conn.cursor()
 
-def trans( word ):
-    data = {
-        # 'from': 'en',
-        # 'to': 'zh-CHS',
-        'doctype': 'json',
-        'type': 'AUTO',
-        'i':word,
-    }
+    # 匹配数据库已有记录时 不区分大小写
+    rel = cursor.execute("SELECT word from WORDS where word = '" + str(word) + "' COLLATE NOCASE").fetchall()
 
-    proxies = {
-        'http': 'http://122.226.57.70:8888', 
-        'https': 'http://122.226.57.70:8888'
-    }
+    print(rel)
 
-    '''
-    'http://112.111.217.6:9999', 
-    'http://171.12.115.199:9999', 
-    'http://80.78.74.133:55443', 
-    'http://110.243.15.92:9999', 
-    'http://81.95.13.207:5836', 
-    'http://180.250.85.204:8181', 
-    '''
+    status = len(rel)
 
-     
-    url = "http://fanyi.youdao.com/translate"
-    try:
-        r = requests.get(url, params=data, proxies = proxies, timeout = 30)
-        # r = requests.get(url, params=data, timeout = 30)
-        result = r.json()
+    conn.close()
 
-        # 打印翻译结果
-        # 简单结果
-        simple_rel = result['translateResult'][0][0]['tgt']
-        # 完整结果
-        # full_rel = result['smartResult']['entries']
-
-        # return {'simple_rel': simple_rel, 'full_rel': full_rel}
-        return {'simple_rel': simple_rel, }
-    except Exception as e:
-        return {}
+    return status
 
 
-def final_2_file(w, i_cnt):
-    with open('final.txt', 'a') as f:
-        # for w in word_list:
-        f.write('------------------------ ' + str(i_cnt) + '\n')
-        f.write('WORD: ' + w['word'])
+'''
+将该词插入基数据库中
+'''
+def insert_to_base(word):
+    conn = sqlite3.connect('kindle_db/vocab_2020_08_21.db')
+    cursor = conn.cursor()
+
+    id_next = len( cursor.execute('SELECT ID FROM WORDS').fetchall() ) + 1
+
+    cursor.execute("INSERT INTO WORDS (id, word) VALUES (" + str(id_next) + ", '" + str(word) + "')")
+
+    conn.commit()
+
+    print('插入单词' + str(word) + '到 基数据库 成功.')
+
+    conn.close()
+
+    return
+
+
+'''
+w: 单词的翻译结果
+w_stem: 单词的词干 lemma
+'''
+def final_2_file(w, w_stem, i_cnt):
+    with open('output/final.txt', 'a') as f:
+        f.write('-------------------------------------- ' + str(i_cnt) + '\n')
+        f.write(w['word'])
+        if w['phonetic']:
+            f.write('  [' + w['phonetic'] + ']')
         f.write('\n')
-        if w['stem']:
-            f.write('STEM: ' + w['stem'])
+        if w_stem:
+            f.write('lemma: ' + str(w_stem))
             f.write('\n')
-        f.write('TRAN: ' + w['translate'])
-        # f.write('\n')
-        # f.write(w['timestamp'])
+        '''
+        if w['collins']:
+            f.write('collins: ' + str(w['collins']) )
+            f.write('   ')
+            if w['oxford']:
+                f.write('oxford: ' + str(w['oxford']) )
+                f.write('\n')
+            else:
+                f.write('\n')
+        '''
+
+        '''
+        bnc_frq_cnt = 0
+        if int(w['bnc']):
+            f.write('BNC: ' + str(w['bnc']) + '  ' )
+            bnc_frq_cnt += 1
+        if int(w['frq']):
+            f.write('FRQ: ' + str(w['frq']) )
+            bnc_frq_cnt += 1
+        if bnc_frq_cnt:
+            f.write('\n')
+        '''
+
+        if w['tag']:
+            f.write('TAG: ' + w['tag'])
+            f.write('\n')
+        f.write(w['translation'])
         f.write('\n')
+        '''
+        if w['exchange']:
+            f.write('EXC: ' + w['exchange'])
+            f.write('\n')
+        '''
 
 
 
 if __name__ == '__main__':
+    do_check = int(input('需要检测单词是否存在基础词库？ (0/1) : '))
+
+    '''
+    while 1:
+        w_word = input('word:')
+        rel = is_stored(w_word)
+        if rel:
+            print('存在')
+        else:
+            print('不存在')
+            insert_to_base(w_word)
+        print()
+    '''
+
+    # 调用 ECDICT
+    sd = stardict.StarDict('full_dic.db', False)
+    # 查询词干 用 
+    lemma = stardict.LemmaDB()
+    lemma.load('lemma.en.txt')
+
     # 翻译出错的单词列表
     error_list = []
 
     # word_list = get_voca()[:5]
-    word_list = get_voca()[1427:1501]
+    word_list = get_voca(do_check)
 
-    i_cnt = 1427
+    i_cnt = 0
     for word in word_list:
-        # 翻译重试次数
-        r_tran = 3
-        # if i_cnt > 5:
+        # if i_cnt > 100:
         #     break
+        print( str(i_cnt) + ' 翻译 ' + word)
 
-        print( str(i_cnt) + ' 翻译 ' + word['word'])
-        time.sleep(0.5)
-        rel = trans(word['word'])
+        rel = sd.query(word)
         if rel:
-            word['translate'] = rel['simple_rel']
+            # 查询词干 lemma
+            '''
+            w_stem = lemma.word_stem(word)
+            if w_stem:
+                if w_stem[2:-2] == word:
+                    w_stem = None
+            '''
             # write to file
-            final_2_file(word, i_cnt)
+            # final_2_file(rel, w_stem, i_cnt)
+            final_2_file(rel, '', i_cnt)
         else:
-            # 重试
-            if r_tran:
-                r_tran -= 1
-                time.sleep(0.5)
-                rel = trans(word['word'])
-                if rel:
-                    word['translate'] = rel['simple_rel']
-                    # write to file
-                    final_2_file(word, i_cnt)
-            else:
-                print('翻译 ' + word['word'] + '出错了')
-                word['translate'] = 'null'
-                error_list.append(word['word'])
-
+            error_list.append(word)
         i_cnt += 1
 
     for e in error_list:
@@ -138,4 +187,8 @@ if __name__ == '__main__':
 
     print('Writting to file...')
     print('All completed!')
+
+    if len(error_list):
+        for w in error_list:
+            print(w)
 
